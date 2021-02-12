cf7287bca09be103e39a2e2e8aa7d3582813a848
Nina Matti <ninadesireematti@gmail.com>
2021-02-12 19:26:12 +0100

    Imported components into App.js and added routing & main paths as well as linked buttons

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Shelf from './components/Shelf';
+import Book from './components/Book';
+import Profile from './components/Profile';
+import Noteview from './components/Noteview';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <BrowserRouter>
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-          <Route path="/shelf">
-            <Home />
-          </Route>
-          <Route path="/book">
-            <Home />
-          </Route>
-          <Route path="/note">
-            <Home />
-          </Route>
-          <Route path="/profile">
-            <Home />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <section className="main">
+        <BrowserRouter>
+          <div className="sidenav nav-list">
+            <button className="btn"><Link to="/home">Home</Link></button>
+            <button className="btn"><Link to="/shelf">Shelf</Link></button>
+            <button className="btn"><Link to="/book">Book</Link></button>
+            <button className="btn"><Link to="/note">Note</Link></button>
+            <button className="btn"><Link to="/profile">Profile</Link></button>
+          </div>
+          <Switch>
+            <Route path="/home" >
+              <Home />
+            </Route>
+            <Route path="/shelf">
+              <Shelf />
+            </Route>
+            <Route path="/book">
+              <Book />
+            </Route>
+            <Route path="/note">
+              <Noteview />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+        
+      </section>
       <Footer />
     </div>
   );
