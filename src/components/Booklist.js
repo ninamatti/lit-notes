89b92499906e32903a1726305af2bc22f907f42e
Nina Matti <ninadesireematti@gmail.com>
2021-02-15 21:29:41 +0100

    Created dynamic book rendering for all books that are in a given list

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import Book from './BookListview'
+import { useSelector } from 'react-redux';
 
-function Booklist() {
+
+function Booklist({ bookIds }) {
+    const bookData = useSelector(state => state.books);
+    const listLength = bookIds.length;
+
+
     return (
         <div className="booklist-container">
             <h1>List of Books</h1>
             <div className="book-list-filtered">
-                <Book />
+            {bookData.map((book, index) => {
+                for(let i = 0; i < listLength; i++) {
+                    if(book.id === bookIds[i]) {
+                        return (
+                            <Book bookIndex={index}/>
+                        ) 
+                    }
+                }  
+            })}
             </div>
         </div>
     )
