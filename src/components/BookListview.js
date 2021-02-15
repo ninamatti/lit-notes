d9baa7901757ff919577b8258b59ce63e993421e
Nina Matti <ninadesireematti@gmail.com>
2021-02-15 21:30:16 +0100

    Made small text change to differentiate between book listview and book singleview

--- src/components/BookListview.js
+++ src/components/BookListview.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Book() {
     return (
         <div>
-            <h1>This is the book component, shows info and notes list of one book.</h1>
+            <h1>This is the book component, shows info card of one book</h1>
         </div>
     )
 }
