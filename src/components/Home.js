91bf5b0542730ee113b4128ec3ffb8b056c9dd3a
Nina Matti <ninadesireematti@gmail.com>
2021-02-15 21:29:04 +0100

    Implemented active shelf change on button press

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import { useDispatch } from 'react-redux';
+import { changeActive } from '../actions';
 
 function Home() {
+    const dispatch = useDispatch();
+
     return (
         <div className="home-container">
             <h1 className="home-container-title">MY BOOKSHELVES</h1> 
-            <button className="book-shelf-btn current">Currently reading</button>
-            <button className="book-shelf-btn future">Want to read</button>
-            <button className="book-shelf-btn past">Read</button>
+            <button className="book-shelf-btn current"
+                    onClick={() => {
+                        dispatch(changeActive(0));
+                    }}
+                    >Currently reading</button>
+            <button className="book-shelf-btn future"
+                    onClick={() => {
+                        dispatch(changeActive(1));
+                    }}
+                    >Want to read</button>
+            <button className="book-shelf-btn past"
+                    onClick={() => {
+                        dispatch(changeActive(2));
+                    }}
+                    >Read</button>
         </div>
     )
 }
