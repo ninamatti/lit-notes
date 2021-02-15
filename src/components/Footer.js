22094990d7aa287a2b4644450e7dec036cc4ade0
Nina Matti <ninadesireematti@gmail.com>
2021-02-15 21:28:43 +0100

    Changed text in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div>
-            <footer>Footing this thing</footer>
+            <footer>Ye mighty Footer</footer>
         </div>
     )
 }
