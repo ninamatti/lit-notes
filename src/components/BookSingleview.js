84d240a5eaffdcbb23800c4424adef1e58ece26e
Nina Matti <ninadesireematti@gmail.com>
2021-02-15 21:30:31 +0100

    Created book singleview component

---
+++ src/components/BookSingleview.js
@@ -0,0 +1,20 @@
+import { useSelector } from 'react-redux';
+
+function BookSingleview(bookIndex = 0) {
+    const bookData = useSelector( state => state.books);
+
+    return (
+        <div>
+            {bookData.map((book) => {
+                return (
+                    <h2>Title: {book.title}</h2>
+                )   
+            })}
+            
+        </div>
+    )
+}
+
+export default BookSingleview
+
+
