9c7be7650789577955a43d386337ef6cd42dfc82
Nina Matti <ninadesireematti@gmail.com>
2021-02-16 12:00:48 +0100

    Adjusted home component to incorporate additional actions

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,46 @@
 import { useDispatch } from 'react-redux';
-import { changeActive } from '../actions';
+import { changeActiveShelf } from '../actions';
+import { useHistory } from 'react-router-dom';
+
 
 function Home() {
     const dispatch = useDispatch();
+    let history = useHistory();
+
+    function changeToShelfview() {
+        history.push("/shelf");
+    }
 
     return (
         <div className="home-container">
-            <h1 className="home-container-title">MY BOOKSHELVES</h1> 
-            <button className="book-shelf-btn current"
-                    onClick={() => {
-                        dispatch(changeActive(0));
-                    }}
-                    >Currently reading</button>
-            <button className="book-shelf-btn future"
-                    onClick={() => {
-                        dispatch(changeActive(1));
-                    }}
-                    >Want to read</button>
-            <button className="book-shelf-btn past"
-                    onClick={() => {
-                        dispatch(changeActive(2));
-                    }}
-                    >Read</button>
+            <h1 className="home-container-title">My Bookshelves</h1> 
+            <div className="home-container_content-wrapper">
+                <div className="home-container_left">
+                    <button className="book-shelf-btn current"
+                            onClick={() => {
+                                dispatch(changeActiveShelf(0));
+                                changeToShelfview();
+                            }}
+                            >Currently reading</button>
+                    <button className="book-shelf-btn future"
+                            onClick={() => {
+                                dispatch(changeActiveShelf(1));
+                                changeToShelfview();
+                            }}
+                            >Want to read</button>
+                    <button className="book-shelf-btn past"
+                            onClick={() => {
+                                dispatch(changeActiveShelf(2));
+                                changeToShelfview();
+                            }}
+                            >Read</button>
+                </div>
+                <div className="home-container_right">
+                    <img src="images/undraw_reading4.png" className="home-img"/>
+                </div>
+
+            </div>
+            
         </div>
     )
 }
