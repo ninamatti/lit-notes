3ebc86e04dd875d592054518b8be4f92675f81e6
Nina Matti <ninadesireematti@gmail.com>
2021-02-20 20:07:29 +0100

    Added isLoggedIn state, firebase authentication and Login component

--- src/App.js
+++ src/App.js
@@ -6,42 +6,70 @@ import Shelf from './components/Shelf';
 import Book from './components/BookSingleview';
 import Profile from './components/Profile';
 import NoteScrollview from './components/NoteScrollview';
+import Login from './components/session/Login.jsx';
 import './App.css';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import fire from './fire.js';
 
 function App() {
   const activeShelf = useSelector(state => state.activeShelfId);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  fire.auth().onAuthStateChanged((user) => {
+    return user ? setIsLoggedIn(true) : setIsLoggedIn(false);
+  });
+
+  console.log('logged in?', isLoggedIn);
 
   return (
     <div className="App">
-      <Header />
+      <Header loginStatus={isLoggedIn}/>
       <section className="main">
         <BrowserRouter>
-          <div className="sidenav nav-list">
-            <button className="btn"><Link to="/home" className="nav-link">HOME</Link></button>
-            <button className="btn"><Link to="/shelf" className="nav-link">SHELF</Link></button>
-            <button className="btn"><Link to="/book" className="nav-link">BOOK</Link></button>
-            <button className="btn"><Link to="/note" className="nav-link">NOTE</Link></button>
-            <button className="btn"><Link to="/profile" className="nav-link">PROFILE</Link></button>
-          </div>
-          {/* <div className="v-divider"></div> */}
-          <Switch>
-            <Route path="/home" >
-              <Home />
-            </Route>
-            <Route path="/shelf">
-              <Shelf  shelfName={activeShelf}/>
-            </Route>
-            <Route path="/book">
-              <Book />
-            </Route>
-            <Route path="/note">
-              <NoteScrollview />
-            </Route>
-            <Route path="/profile">
-              <Profile />
-            </Route>
-          </Switch>
+          
+          {!isLoggedIn ?
+            (
+            <Switch>
+              <Route path="/">
+                <Login />
+              </Route>
+            </Switch>
+            )
+            :
+            (
+            <>          
+            <div className="sidenav nav-list">
+              <button className="btn"><Link to="/home" className="nav-link">HOME</Link></button>
+              <button className="btn"><Link to="/shelf" className="nav-link">SHELF</Link></button>
+              <button className="btn"><Link to="/book" className="nav-link">BOOK</Link></button>
+              <button className="btn"><Link to="/note" className="nav-link">NOTE</Link></button>
+              <button className="btn"><Link to="/profile" className="nav-link">PROFILE</Link></button>
+            </div>
+            <Switch>
+              <Route path="/" >
+                <Home />
+              </Route>
+              <Route path="/home" >
+                <Home />
+              </Route>
+              <Route path="/shelf">
+                <Shelf  shelfName={activeShelf}/>
+              </Route>
+              <Route path="/book">
+                <Book />
+              </Route>
+              <Route path="/note">
+                <NoteScrollview />
+              </Route>
+              <Route path="/profile">
+                <Profile />
+              </Route>
+            </Switch>
+            </>
+            )
+          }
+          
         </BrowserRouter>
         
       </section>
