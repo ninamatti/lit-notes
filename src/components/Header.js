814c1302a5a83b4a1a5755d7ec28538f73c5080a
Nina Matti <ninadesireematti@gmail.com>
2021-02-20 20:07:42 +0100

    Added logout button to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
+import fire from '../fire.js';
 
+function Header( { loginStatus }) {
+    const signOut = () => {
+        fire.auth().signOut()
+    };
 
-function Header() {
     return (
         <div>
             <header>
-                <h1>LIT NOTES</h1>
-                <h3>What are you reading today?</h3>
+                <div className="logo-wrapper">
+                    <h1>LIT NOTES</h1>
+                    <h3>What are you reading today?</h3>
+                </div>
+                { { loginStatus } ?
+                    <button className="btn-logout" onClick={signOut}>LOGOUT</button>
+                    : ""
+                }
             </header>
         </div>
     )
