19c25b023025092d61ac48f031b7d1fdf2be8017
Nina Matti <ninadesireematti@gmail.com>
2021-03-07 15:46:34 +0100

    Created get request for all note entries

--- src/services/noteServices.js
+++ src/services/noteServices.js
@@ -3,6 +3,7 @@ import fire from '../fire';
 
 const url = 'http://localhost:8080/api';
 
+// create user auth token
 const createToken = async () => {
     const user = fire.auth().currentUser;
     const token = user && (await user.getIdToken());
@@ -15,10 +16,31 @@ const createToken = async () => {
     return payloadHeader;
 }
 
+// post request for new note
 export const addToNotes = (title, content) => {
-  const payload = {
-    title,
-    content,
-  }
-  axios.post(url, payload);
+    const header = await createToken();
+
+    const payload = {
+        title,
+        content,
+    }
+    try {
+        const res = await axios.post(url, payload, header);
+        return res.data;
+    } catch (e) {
+        console.error(e);
+    }
+
+    axios.post(url, payload);
 };
+
+// get request for all notes
+export const getAllNotes = async () => {
+    const header = await createToken();
+    try {
+        const res = await axios.get(url, header);
+        return res.data;
+    } catch (e) {
+        console.error(e);
+    }
+}
